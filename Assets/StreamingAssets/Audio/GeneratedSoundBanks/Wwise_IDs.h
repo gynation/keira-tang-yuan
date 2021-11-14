df6b1cc6683b51786bd8ccb01a4ab65b0c031d8c
Gwen <gwen@imbainteractive.com>
2021-11-15 00:11:36 +0800

    Voices from Ethan, Husband & Merwyn
    - Cleaned up the voice files
    - Implemented in Wwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,28 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID VO_06INS_030_MERWYN = 410205544U;
+        static const AkUniqueID VO_06INS_050_MERWYN = 1285751974U;
+        static const AkUniqueID VO_06INS_060_MERWYN = 4001995393U;
+        static const AkUniqueID VO_06INS_070_MERWYN = 3094539428U;
+        static const AkUniqueID VO_06INS_100_MERWYN = 3573006388U;
+        static const AkUniqueID VO_08CHF_010_ETHAN = 831783015U;
+        static const AkUniqueID VO_08CHF_020_ETHAN = 333640230U;
+        static const AkUniqueID VO_08CHF_050_ETHAN = 3568377795U;
+        static const AkUniqueID VO_08CHF_070_ETHAN = 1935602725U;
+        static const AkUniqueID VO_08CHF_080_ETHAN = 815836500U;
+        static const AkUniqueID VO_08CHF_090_ETHAN = 2932602975U;
+        static const AkUniqueID VO_08CHF_110_ETHAN = 513475766U;
+        static const AkUniqueID VO_08CHF_120_ETHAN = 296267319U;
+        static const AkUniqueID VO_08CHF_130_ETHAN = 903915116U;
+        static const AkUniqueID VO_09HRW_020_HUSBAND = 2240928088U;
+        static const AkUniqueID VO_11KEI_160_HUSBAND = 2569653828U;
+        static const AkUniqueID VO_11KEI_170_HUSBAND = 3103793179U;
+        static const AkUniqueID VO_11KEI_180_HUSBAND = 3250207694U;
+    } // namespace EVENTS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
